9d8d68ecf1c6f5ace9d668d84d650aad7306528c
nickblantz <nicholasblantz@gmail.com>
2018-12-02 17:49:36 -0500

    Fligh search populating from FlightEase API

---
+++ src/java/com/airaid/pojo/Flight.java
@@ -0,0 +1,13 @@
+/*
+ * Created by Nicholas Blantz on 2018.12.02  * 
+ * Copyright © 2018 Nicholas Blantz. All rights reserved. * 
+ */
+package com.airaid.pojo;
+
+/**
+ *
+ * @author Admin
+ */
+public class Flight {
+    
+}
